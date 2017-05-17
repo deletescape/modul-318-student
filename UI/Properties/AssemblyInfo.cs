fdf298dd0ed4abf5ae1eb99dff7203e54482346b
Till <deletescape@outlook.com>
2017-05-17 16:06:35 +0200

    Add Installer Project

--- UI/Properties/AssemblyInfo.cs
+++ UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UI")]
+[assembly: AssemblyTitle("ÖVÖ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bison IT-Services AG")]
-[assembly: AssemblyProduct("UI")]
+[assembly: AssemblyProduct("ÖVÖ")]
 [assembly: AssemblyCopyright("Copyright © Bison IT-Services AG 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
